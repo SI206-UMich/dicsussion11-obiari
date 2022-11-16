a1bfdad3741cbdf5cf473a8b85c8406bbfd9bd88
Oby Uche <obyuche@Obys-MacBook-Air.local>
2022-11-16 17:43:08 -0500

    completed discussion 11

--- discussion11.py
+++ discussion11.py
@@ -39,13 +39,15 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
+    cur.execute("DROP TABLE IF EXIST Patiens")
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id INTEGER, age INTEGER, cuteness INTEGER, agressiveness INTEGER")
+    conn.commit()
 
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
-    
+    cur.execute("INSERT INTO Patients (pen_id, name, spaces_id, age, cuteness, agressiveness) VALUES(?,?,?,?,?,?)", (0, 'Fluffle', 0, 3, 90, 100))
+    conn.commit()
 
 # TASK 2
 # CODE TO ADD JSON TO THE TABLE
@@ -57,15 +59,36 @@ def add_pets_from_json(filename, cur, conn):
     file_data = f.read()
     f.close()
     json_data = json.loads(file_data)
-
     # THE REST IS UP TO YOU
-    pass
+    pet_id = 1
+    for pet in json_data:
+        name = pet['name']
+        age = pet['age']
+        species = pet['species']
+        cuteness = pet ['cuteness']
+        aggressiveness = pet ['aggressiveness']
+        cur.execute('SELECT id FROM Species WHERE title = ?', (species,)) #comme means tuple
+        # DO NOT USE cur.execute(f'SELECT id FROM Species WHERE title = {species})
+        # when species = 'Rabbit\nDROP TABLE
+        spaces_id = cur.fetchone()[0]
 
+        cur.execute("INSERT INTO Patients (pet_id, name, spaces_id, age, cuteness, aggressiveness) VALUES(?,?,?,?,?,?)", (pet_id, name, spaces_id, age, cuteness , aggressiveness))
+        pet_id += 1
+
+        conn.commit()
+
+
+    #cur.execute('DROP TABLE IF EXIST Patients')
+    #cur.execute("CREATE TABLE ")
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
-    pass
+    cur.execute('SELECT name FROM Patients WHERE agressiveness <= ?', (aggressiveness))
+    non_aggressive_pets_list = cur.fetchall() #we use fetach all to get all rows of the select results (list of tups)
+    non_aggressive_pets_list = [item[0] for item in non_aggressive_pets_list] 
+    return non_aggressive_pets_list
+
 
 
 
